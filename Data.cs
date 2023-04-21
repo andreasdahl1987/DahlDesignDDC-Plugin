c083a7114c322ed0ce2cac0258c58f1c689f016b
Andreas Dahl <40788634+andreasdahl1987@users.noreply.github.com>
2023-04-21 23:50:56 +0200

    Working!

--- Data.cs
+++ Data.cs
@@ -8,6 +8,8 @@ using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Forms;
 using System.Windows.Media;
+using System.Windows;
+
 
 namespace DahlDesign.Plugin
 {
@@ -21,39 +23,56 @@ namespace DahlDesign.Plugin
         public ImageSource PictureIcon => this.ToIcon(Properties.Resources.Dahl_icon);
         public string LeftMenuTitle => "DDC";
 
-
+        //Public globals
         public ObservableCollection<string> controllerNames { get; set; } = new ObservableCollection<string> { "dummy"};
         public int selectedItem { get; set; } = -1;
-        public int clutchValue { get; set; } = 0;
-        public int throttleValue { get; set; } = 0;
-        public int brakeValue { get; set; } = 0;
-        public int bitePointValue { get; set; } = 0;
-        public int rotaryFieldValue { get; set; } = 0;
-        public int buttonField { get; set; } = 0;
 
+        //Private globals
+        double clutchValue = 0;
+        double throttleValue = 0;
+        double brakeValue = 0;
+        double bitePointValue = 0;
+        int rotaryField = 0;
+        int buttonField = 0;
+
+        int controllerCount = 0;
+        bool countUpdate = false;
 
         public void DataUpdate(PluginManager pluginManager, ref GameData data)
         {
             List<ACToolsUtilities.JoystickDevice> gameControllers = new List<ACToolsUtilities.JoystickDevice> { };
             gameControllers = (List<ACToolsUtilities.JoystickDevice>) pluginManager.GetConnectedGameControllers();
-            controllerNames.Clear();
 
-            for(int i = 0; i < gameControllers.Count; i++)
+            if (controllerCount != gameControllers.Count)
             {
-                controllerNames.Add(gameControllers[i].Name.Replace("_"," "));
+                controllerCount = gameControllers.Count;
+                countUpdate = true;
             }
+
+            if (countUpdate)
+            {
+                System.Windows.Application.Current.Dispatcher.Invoke(() =>
+                {
+                    controllerNames.Clear();
+                    for (int i = 0; i < gameControllers.Count; i++)
+                    {
+                        controllerNames.Add(gameControllers[i].Name.Replace("_", " "));
+                    }
+                    countUpdate = false;
+                });
+            }
+
 
             if (selectedItem >= 0)
             {
-                clutchValue = gameControllers[selectedItem].LastState.X;
-                clutchValue = gameControllers[selectedItem].LastState.X;
+                clutchValue = Math.Round(gameControllers[selectedItem].LastState.X / 655.35, 1);
+                brakeValue = Math.Round(gameControllers[selectedItem].LastState.RotationZ / 655.35,1);
+                throttleValue = Math.Round(gameControllers[selectedItem].LastState.AccelerationX / 655.35, 1);
+                bitePointValue = Math.Round(gameControllers[selectedItem].LastState.RotationY / 655.35,1);
+
+                buttonField = Convert.ToInt32(gameControllers[selectedItem].LastState.Y);
+                rotaryField = Convert.ToInt32(gameControllers[selectedItem].LastState.Z);
             }
-        }
-
-        public void grabControllerInfo (int selector)
-        {
-
-
         }
         
         public void End(PluginManager pluginManager)
@@ -72,7 +91,44 @@ namespace DahlDesign.Plugin
 
             Settings = this.ReadCommonSettings<DataSettings>("GeneralSettings", () => new DataSettings());
 
-            this.AttachDelegate("CurrentDateTime", () => DateTime.Now);
+            addProps();
+        }
+
+        public void addProps()
+        {
+            //Proper axis
+            this.AttachDelegate("Clutch", () => clutchValue);
+            this.AttachDelegate("Brake", () => brakeValue);
+            this.AttachDelegate("Throtle", () => throttleValue);
+            this.AttachDelegate("BitePoint", () => bitePointValue);
+
+            //Button field
+            this.AttachDelegate("B1", () => buttonField & 0x01);
+            this.AttachDelegate("B2", () => buttonField >> 1 & 0x01);
+            this.AttachDelegate("B3", () => buttonField >> 2 & 0x01);
+            this.AttachDelegate("B4", () => buttonField >> 3 & 0x01);
+            this.AttachDelegate("NeutralActive", () => buttonField >> 4 & 0x01);
+            this.AttachDelegate("ThrottleHoldActive", () => buttonField >> 5 & 0x01);
+            this.AttachDelegate("BrakeMagicActive", () => buttonField >> 6 & 0x01);
+            this.AttachDelegate("QuickSwitchMode", () => buttonField >> 7 & 0x01);
+            this.AttachDelegate("QuickSwitchActive", () => buttonField >> 8 & 0x01);
+            this.AttachDelegate("HandbrakeActive", () => buttonField >> 9 & 0x01);
+            this.AttachDelegate("Preset", () => (buttonField >> 10 & 0x0f) + 1);
+            this.AttachDelegate("NeutralMode", () => buttonField >> 14 & 0x01);
+
+            //Rotary field
+            this.AttachDelegate("R1", () => rotaryField & 0x01);
+            this.AttachDelegate("R2", () => rotaryField >> 1 & 0x01);
+            this.AttachDelegate("R3", () => rotaryField >> 2 & 0x01);
+            this.AttachDelegate("R4", () => rotaryField >> 3 & 0x01);
+            this.AttachDelegate("R5", () => rotaryField >> 4 & 0x01);
+            this.AttachDelegate("R6", () => rotaryField >> 5 & 0x01);
+            this.AttachDelegate("R7", () => rotaryField >> 6 & 0x01);
+            this.AttachDelegate("R8", () => rotaryField >> 7 & 0x01);
+            this.AttachDelegate("DDSMode", () => rotaryField >> 8 & 0x03);
+            this.AttachDelegate("BiteSetting", () => rotaryField >> 10 & 0x03);
+            this.AttachDelegate("ClutchMode", () => rotaryField >> 12 & 0x03);
+            this.AttachDelegate("R15", () => rotaryField >> 14 & 0x01);
         }
     }
 }
--- SettingsControl.xaml.cs
+++ SettingsControl.xaml.cs
@@ -5,26 +5,14 @@ using System.Windows.Media.Media3D;
 
 namespace DahlDesign.Plugin
 {
-    /// <summary>
-    /// Logique d'interaction pour SettingsControlDemo.xaml
-    /// </summary>
     public partial class SettingsControl : UserControl
     {
         public DDC Plugin { get; }
-
-        public List<string> itemsList = new List<string>
-        {
-            "Item 1",
-            "Item 2",
-            "Item 3"
-        };
-
 
         public SettingsControl()
         {
             InitializeComponent();
         }
-
 
         public SettingsControl(DDC plugin) : this()
         {
@@ -32,7 +20,6 @@ namespace DahlDesign.Plugin
             myComboBox.ItemsSource = plugin.controllerNames;
             myComboBox.SelectionChanged += MyComboBox_SelectionChanged;
         }
-
 
         private void MyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
